60831f75ffd07b893a610192cba2df8f52731b49
sauce-kode <dblacktin@gmail.com>
2021-02-11 15:58:11 +0100

    Adds solution for fibonacci and grid traveller problems

---
+++ memoization/gridTraveler.js
@@ -0,0 +1,14 @@
+const gridTraveler = (m, n, memo = {}) => {
+    const key = m + ',' + n
+    if (key in memo) return memo[key]
+    if(m == 1 && n == 1) return 1
+    if (m == 0 || n == 0) return 0
+    memo[key] = gridTraveler(m-1, n, memo) + gridTraveler(m, n-1, memo)
+    return memo[key]
+}
+
+console.log(gridTraveler(1, 1));
+console.log(gridTraveler(2, 3));
+console.log(gridTraveler(3, 2));
+console.log(gridTraveler(3, 3));
+console.log(gridTraveler(18, 18));
